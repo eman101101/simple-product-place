21cf15cd8aec91184b0d75ec4e57f68afb0ee742
Evan Koumarelas <evankoumarelas@hotmail.com>
2025-01-22 00:17:18 -0500

    idk

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
-import Home from './Home.vue'
+import Home from './components/Home.vue'
 
 const router = createRouter({
   history: createWebHistory(),

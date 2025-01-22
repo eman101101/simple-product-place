d2952157277eb594d3fb26e7d0386cbe1cd3b03b
Evan Koumarelas <evankoumarelas@hotmail.com>
2025-01-21 23:37:21 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default {
-  plugins: [vue()]
-}
+export default defineConfig({
+  plugins: [vue()],
+  optimizeDeps: {
+    include: ['vue']
+  }
+})
